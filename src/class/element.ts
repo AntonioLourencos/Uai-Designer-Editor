89ce737c61b938bbce5cc003a451473f0af829ce
drelocatelli <andressalocatelli@hotmail.com>
2023-04-18 21:45:27 -0300

    resizing elements

--- src/class/element.ts
+++ src/class/element.ts
@@ -6,6 +6,41 @@ class DefineElement {
     constructor(target: string) {
         this.target = target;
         this.draggable();
+        this.resizable();
+    }
+
+    resizable() {
+        interact(this.target).resizable({
+            modifiers: [
+                interact.modifiers.snap({
+                    targets: [interact.snappers.grid({ x: 10, y: 10 })],
+                    range: Infinity,
+                    relativePoints: [{ x: 0, y: 0 }],
+                    offset: 'start',
+                }),
+                interact.modifiers.restrict({
+                    restriction: 'parent',
+                    endOnly: true,
+                    elementRect: { left: 0, right: 1, top: 0, bottom: 1 },
+                }),
+            ],
+            listeners: {
+                move: function (event) {
+                    let { x, y } = event.target.dataset;
+
+                    x = (parseFloat(x) || 0) + event.deltaRect.left;
+                    y = (parseFloat(y) || 0) + event.deltaRect.top;
+
+                    Object.assign(event.target.style, {
+                        width: `${event.rect.width}px`,
+                        height: `${event.rect.height}px`,
+                    });
+
+                    Object.assign(event.target.dataset, { x, y });
+                },
+            },
+            edges: { left: true, right: true, bottom: true, top: true },
+        });
     }
 
     draggable() {
