a80331aa7db2331016f560734ab0a10c9f177cb6
drelocatelli <andressalocatelli@hotmail.com>
2023-04-19 18:48:39 -0300

    save element CSS

--- src/class/element.ts
+++ src/class/element.ts
@@ -10,11 +10,24 @@ class DefineElement {
         this.draggable();
     }
 
+    saveElementChanges(id: number, style: CSSStyleDeclaration) {
+        const cssProperties: any = {};
+        for (let i = 0; i < style.length; i++) {
+            const propertyName = style[i];
+            cssProperties[propertyName] = style.getPropertyValue(propertyName);
+        }
+        useElementsStore().setElementStyle(id, cssProperties);
+    }
+
     draggable() {
         const targetEl = document.querySelector(this.target) as HTMLDivElement;
         resizer.add(targetEl, {
             minWidth: 5,
             minHeight: 5,
+            onDragEnd: (e: DragEvent) => {
+                const target = (e as any).evtTarget as HTMLDivElement;
+                this.saveElementChanges(parseInt(target.dataset.id!), target.style);
+            },
         });
     }
 }
