1064fbc0a87e0f73c8ea9bad1e4a1a63463c0fef
drelocatelli <andressalocatelli@hotmail.com>
2023-04-20 12:38:17 -0300

    improving panel

--- src/components/panels/bottom/bottom.vue
+++ src/components/panels/bottom/bottom.vue
@@ -1,8 +1,29 @@
 <script lang="ts" setup>
+import './bottom.scss';
+import { ref } from 'vue';
+
+
+const isOpen = ref<boolean>(false);
+
+const setIsOpen = () => {
+    isOpen.value = !isOpen.value;
+}
+
 </script>
 
 <template>
     <div class="bottom panel">
-        bottom
+        <div class="title" :onclick="setIsOpen">
+            <template :style="{display: isOpen ? 'inline-block': 'none'}">
+                <i class="fas fa-caret-down"></i>
+            </template>
+            <template :style="{display: !isOpen ? 'inline-block': 'none'}">
+                <i class="fas fa-caret-up"></i>
+            </template>
+            &nbsp;Linha do tempo
+        </div>
+        <div class="content" :style="{display: isOpen ? 'block' : 'none'}">
+            content
+        </div>
     </div>
 </template>
