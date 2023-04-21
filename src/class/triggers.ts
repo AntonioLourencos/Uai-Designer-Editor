ae04f0c0de57ce543704b077a51cecead2edadca
drelocatelli <andressalocatelli@hotmail.com>
2023-04-21 17:40:49 -0300

    fix state after select

--- src/class/triggers.ts
+++ src/class/triggers.ts
@@ -15,7 +15,6 @@ class Trigger extends Environment {
         this.stage = this.initStage();
         this.layer = this.newLayer();
         this.shapeSelection();
-        this.textShapeSelection();
         this.listenEvents();
     }
 
@@ -99,172 +98,13 @@ class Trigger extends Environment {
         return shape;
     }
 
-    textShapeSelection() {
-        // insert transform rotate, resize
-        const MIN_WIDTH = 20;
-        const tr = new Konva.Transformer({
-            nodes: [],
-            padding: 5,
-            // enable only side anchors
-            enabledAnchors: ['middle-left', 'middle-right'],
-            // limit transformer size
-            boundBoxFunc: (oldBox, newBox) => {
-                if (newBox.width < MIN_WIDTH) {
-                    return oldBox;
-                }
-                return newBox;
-            },
-        });
-        this.layer.add(tr);
-
-        tr.nodes([]);
-
-        let selectionRectangle = new Konva.Rect({
-            fill: '#D9EAF6',
-            stroke: '#0D99FF',
-            visible: false,
-            strokeWidth: 1,
-        });
-        this.layer.add(selectionRectangle);
-
-        let isPainting = false;
-
-        const stopSelect = () => {
-            tr.nodes([]);
-            selectionRectangle.visible(false);
-        };
-
-        // prevent selection on paint mode
-        useStatusStore().$subscribe((mutation, value) => {
-            isPainting = value.paint.paintMode;
-        });
-
-        // blur events
-        useFocusStore().$subscribe((mutation, value) => {
-            const focusedEl = value.lastFocusElement as HTMLDivElement;
-            if (focusedEl.id != 'environment') {
-                const isCanvas = focusedEl.classList.contains('konvajs-content') || focusedEl.localName == 'canvas';
-                if (!isCanvas) {
-                    stopSelect();
-                }
-            }
-        });
-
-        var x1: number, y1: number, x2, y2;
-
-        this.stage.on('mousedown touchstart', (e) => {
-            // do nothing if we mousedown on any shape
-            if (isPainting || e.target !== this.stage) {
-                return;
-            }
-            e.evt.preventDefault();
-            x1 = this.stage.getPointerPosition()!.x;
-            y1 = this.stage.getPointerPosition()!.y;
-            x2 = this.stage.getPointerPosition()!.x;
-            y2 = this.stage.getPointerPosition()!.y;
-
-            selectionRectangle.visible(true);
-            selectionRectangle.width(0);
-            selectionRectangle.height(0);
-        });
-
-        this.stage.on('mousemove touchmove', (e) => {
-            // do nothing if we didn't start selection
-            if (isPainting || !selectionRectangle.visible()) {
-                return;
-            }
-            e.evt.preventDefault();
-            x2 = this.stage.getPointerPosition()!.x;
-            y2 = this.stage.getPointerPosition()!.y;
-
-            selectionRectangle.setAttrs({
-                x: Math.min(x1, x2),
-                y: Math.min(y1, y2),
-                width: Math.abs(x2 - x1),
-                height: Math.abs(y2 - y1),
-            });
-        });
-
-        this.stage.on('mouseup touchend', (e) => {
-            // do nothing if we didn't start selection
-            if (isPainting || !selectionRectangle.visible()) {
-                return;
-            }
-            e.evt.preventDefault();
-            // update visibility in timeout, so we can check it in click event
-            setTimeout(() => {
-                selectionRectangle.visible(false);
-            });
-
-            var shapes = this.stage.find('.textShape');
-            var box = selectionRectangle.getClientRect();
-            var selected = shapes.filter((shape) => Konva.Util.haveIntersection(box, shape.getClientRect()));
-            tr.nodes(selected);
-
-            useFocusStore().setActionShape({ name: 'Ações' });
-
-        });
-
-        // clicks should select/deselect shapes
-        const stageRef = this.stage;
-        this.stage.on('mousedown click tap', function (e) {
-            const texts = stageRef.find('Text');
-            texts.forEach((text) => {
-                text.on('transform', () => {
-                    // with enabled anchors we can only change scaleX
-                    // so we don't need to reset height
-                    // just width
-                    text.setAttrs({
-                        width: Math.max(text.width() * text.scaleX(), MIN_WIDTH),
-                        scaleX: 1,
-                        scaleY: 1,
-                    });
-                });
-                // if we are selecting with rect, do nothing
-                if (selectionRectangle.visible()) {
-                    return;
-                }
-
-                if (isPainting || e.target === stageRef) {
-                    tr.nodes([]);
-                    return;
-                }
-
-                // do nothing if clicked NOT on our rectangles
-                if (!e.target.hasName('textShape')) {
-                    return;
-                }
-
-                // do we pressed shift or ctrl?
-                const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
-                const isSelected = tr.nodes().indexOf(e.target) >= 0;
-
-                if (!metaPressed && !isSelected) {
-                    // if no key pressed and the node is not selected
-                    // select just one
-                    tr.nodes([e.target]);
-                } else if (metaPressed && isSelected) {
-                    // if we pressed keys and node was selected
-                    // we need to remove it from selection:
-                    const nodes = tr.nodes().slice(); // use slice to have new copy of array
-                    // remove node from array
-                    nodes.splice(nodes.indexOf(e.target), 1);
-                    tr.nodes(nodes);
-                } else if (metaPressed && !isSelected) {
-                    // add the node into selection
-                    const nodes = tr.nodes().concat([e.target]);
-                    tr.nodes(nodes);
-                }
-
-                // when click shape
-                useFocusStore().setActionShape({ name: 'Propriedades', shape: text });
-            });
-        });
-    }
-
     shapeSelection() {
         // insert transform rotate, resize
         const tr = new Konva.Transformer();
+        const stageRef = this.stage;
+        const focusStore = useFocusStore();
+
+
         this.layer.add(tr);
 
         tr.nodes([]);
@@ -354,45 +194,46 @@ class Trigger extends Environment {
         });
 
         // clicks should select/deselect shapes
-        const stageRef = this.stage;
-        this.stage.on('mousedown click tap', function (e) {
-            // if we are selecting with rect, do nothing
-            if (selectionRectangle.visible()) {
-                return;
-            }
-
-            if (isPainting || e.target === stageRef) {
-                tr.nodes([]);
-                return;
-            }
-
-            // do nothing if clicked NOT on our rectangles
-            if (!e.target.hasName('shape')) {
-                return;
-            }
-
-            // do we pressed shift or ctrl?
-            const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
-            const isSelected = tr.nodes().indexOf(e.target) >= 0;
-
-            if (!metaPressed && !isSelected) {
-                // if no key pressed and the node is not selected
-                // select just one
-                tr.nodes([e.target]);
-            } else if (metaPressed && isSelected) {
-                // if we pressed keys and node was selected
-                // we need to remove it from selection:
-                const nodes = tr.nodes().slice(); // use slice to have new copy of array
-                // remove node from array
-                nodes.splice(nodes.indexOf(e.target), 1);
-                tr.nodes(nodes);
-            } else if (metaPressed && !isSelected) {
-                // add the node into selection
-                const nodes = tr.nodes().concat([e.target]);
-                tr.nodes(nodes);
-            }
-
-            useFocusStore().setActionShape({ name: 'Propriedades', shape: e.target });
+        this.stage.on('mousedown click tap', async function (e) {
+            focusStore.setActionShape({ name: 'Propriedades', shape: e.target });
+            setTimeout(() => {
+                // if we are selecting with rect, do nothing
+                if (selectionRectangle.visible()) {
+                    return;
+                }
+    
+                if (isPainting || e.target === stageRef) {
+                    tr.nodes([]);
+                    return;
+                }
+    
+    
+                // do nothing if clicked NOT on our rectangles
+                if (!e.target.hasName('shape')) {
+                    return;
+                }
+    
+                // do we pressed shift or ctrl?
+                const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
+                const isSelected = tr.nodes().indexOf(e.target) >= 0;
+    
+                if (!metaPressed && !isSelected) {
+                    // if no key pressed and the node is not selected
+                    // select just one
+                    tr.nodes([e.target]);
+                } else if (metaPressed && isSelected) {
+                    // if we pressed keys and node was selected
+                    // we need to remove it from selection:
+                    const nodes = tr.nodes().slice(); // use slice to have new copy of array
+                    // remove node from array
+                    nodes.splice(nodes.indexOf(e.target), 1);
+                    tr.nodes(nodes);
+                } else if (metaPressed && !isSelected) {
+                    // add the node into selection
+                    const nodes = tr.nodes().concat([e.target]);
+                    tr.nodes(nodes);
+                }
+            }, 100);
         });
     }
 }
