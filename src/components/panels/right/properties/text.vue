6fee6829e92f3543cb92d745513236f5fadd5e63
drelocatelli <andressalocatelli@hotmail.com>
2023-04-21 16:25:05 -0300

    changing text

--- src/components/panels/right/properties/text.vue
+++ src/components/panels/right/properties/text.vue
@@ -4,10 +4,18 @@ import useFocusStore from '../../../../store/focus';
 
 const focusStore = useFocusStore();
 const shape = computed(() => focusStore.action!.shape!);
+
+const changeText = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    shape.value.setAttr('text', target.value);
+};
 </script>
 
 <template>
     <div class="textProperties">
-        {{ shape.className }}
+        <div class="label">
+            <h5>Alterar texto</h5>
+            <input type="text" :value="shape.attrs.text" :onblur="changeText" :onkeyup="(e: KeyboardEvent) => {if(e.key == 'Enter') changeText(e)}" />
+        </div>
     </div>
 </template>
